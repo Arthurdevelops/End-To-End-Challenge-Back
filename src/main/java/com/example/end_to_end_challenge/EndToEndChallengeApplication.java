9860fba1328c9826c3064a7b511b07a289132ed4
Arthur FRANCOIS <arthur.francois2504@gmail.com>
2025-06-18 15:54:54 +0200

    init project

---
+++ src/main/java/com/example/end_to_end_challenge/EndToEndChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.end_to_end_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndToEndChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndToEndChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/end_to_end_challenge/EndToEndChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.end_to_end_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndToEndChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
