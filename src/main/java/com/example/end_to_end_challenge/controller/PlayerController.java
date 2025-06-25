f9bd239ad90902f8afd9723fe0302aa4afca42e3
Arthur FRANCOIS <arthur.francois2504@gmail.com>
2025-06-25 11:29:50 +0200

    refacto user into player and update database attributes for player table

---
+++ src/main/java/com/example/end_to_end_challenge/controller/PlayerController.java
@@ -0,0 +1,35 @@
+package com.example.end_to_end_challenge.controller;
+
+import com.example.end_to_end_challenge.dto.PlayerDTO;
+import com.example.end_to_end_challenge.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*")
+public class PlayerController {
+
+    @Autowired
+    private PlayerService playerService;
+
+    @GetMapping
+    public ResponseEntity<List<PlayerDTO>> getUsers() {
+        return ResponseEntity.ok(playerService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PlayerDTO> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(playerService.getUserById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<PlayerDTO> createUser(@RequestBody PlayerDTO userDTO) {
+        PlayerDTO createdUser = playerService.createUser(userDTO);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+}
